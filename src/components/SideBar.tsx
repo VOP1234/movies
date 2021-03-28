33b23a3384f39d30036171e9ac0004c24cd9e6e1
Vitor Pereira <vop1234@hotmail.com>
2021-03-27 22:20:28 -0300

    Completion

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+import { useMemo, useState } from "react";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  clickButton(id: number): void
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+}
+
+export function SideBar({ clickButton, genres, selectedGenreId }: SideBarProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => clickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
